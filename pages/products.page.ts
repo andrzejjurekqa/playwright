22550a3ece3f94b9c23988eba2e73b6bcc62f03f
AJ <andrzej.jurek.qa@gmail.com>
2024-02-03 17:30:56 +0100

    login corr products visible();

--- pages/products.page.ts
+++ pages/products.page.ts
@@ -3,6 +3,7 @@ import { Page, Locator } from "@playwright/test";
 export class ProductPage {
     constructor(page) {
         this.page = page;
-        this.shoppingCart = page.locator('shopping_cart_container');
+        this.shoppingCart = page.locator('#shopping_cart_container');
+        this.sortingButton = page.locator('.product_sort_container');
     }
 }
--- tests/products.spec.ts
+++ tests/products.spec.ts
@@ -4,9 +4,21 @@ import { ProductPage } from '../pages/products.page';
 
 test.describe('Verify products page', () => {
     let loginPage;
+    let productPage;
     test.beforeEach(async ({page}) => {
         loginPage = new LoginPage(page);
+        productPage = new ProductPage(page);
         await page.goto('https://www.saucedemo.com');
         await loginPage.login('problem_user', 'secret_sauce');
-    });
+    })
+    test.afterAll(async () =>{
+        console.log('Done');
+    })
+
+    test('Verify product page', async ({page}) => {
+        const title = page.locator('.title');
+        await expect(title).toHaveText('Products');
+        await expect(productPage.sortingButton).toBeVisible();
+        await expect(productPage.shoppingCart).toBeVisible();
+    })
 })
