0a17706ece9a2753dcd24319eca1230f823fe10f
AJ <andrzej.jurek.qa@gmail.com>
2024-02-03 15:37:51 +0100

    login done product start

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -7,17 +7,10 @@ export class LoginPage {
         this.passwordField = page.locator('#password');
         this.loginButton = page.locator('#login-button');
         this.errorMessage = page.locator('h3[data-test=error]');
-        this.title = page.locator('.title');
     }
     async login(username:string, password:string):Promise<void>{
         await this.usernameField.type(username);
         await this.passwordField.type(password);
         await this.loginButton.click();
-    }
-    async error(){
-        await this.errorMessage.toHaveText(/Username and password do not match any user in this service/);
-    }
-    async title(){
-        await this.title.toHaveText('Products');
     }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,17 +6,22 @@ test.describe('Verify the Login screen', () => {
         await page.goto('https://www.saucedemo.com');
     });
 
-    test('Verify the title', async ({page}) => {
+    test('Verify the login page', async ({page}) => {
+        const loginPage = new LoginPage(page);
         await expect(page).toHaveTitle('Swag Labs');
+        await expect(loginPage.usernameField).toBeVisible();
+        await expect(loginPage.passwordField).toBeVisible();
+        await expect(loginPage.loginButton).toBeVisible();
     });
     test('Incorrect login', async ({page}) => {
         const loginPage = new LoginPage(page);
         await loginPage.login('asdasd', 'sdaasddas');
-        await LoginPage.error;
+        await expect(loginPage.errorMessage).toHaveText(/Username and password do not match any user in this service/);
     });
     test('Correct login', async ({page}) => {
         const loginPage = new LoginPage(page);
         await loginPage.login('problem_user', 'secret_sauce');
-        await LoginPage.title;
+        const title = page.locator('.title');
+        await expect(title).toHaveText('Products');
     });
 });
