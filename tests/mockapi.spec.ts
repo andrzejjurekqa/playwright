c192eb02fe6500d40641ac5a374228d8feaddfea
AJ <andrzej.jurek.qa@gmail.com>
2024-02-06 22:49:18 +0100

    Changes to be committed: 	modified:   tests/mockapi.spec.ts 	new file:   tests/reqres-mock.spec.ts 	renamed:    tests/nomockapi.spec.ts -> tests/reqres-nomock.spec.ts 	renamed:    tests/login.spec.ts -> tests/sauce-login.spec.ts 	renamed:    tests/products.spec.ts -> tests/sauce-products.spec.ts

--- tests/mockapi.spec.ts
+++ tests/mockapi.spec.ts
@@ -1,24 +1,18 @@
 import { test, expect } from '@playwright/test';
-import { data } from '../test-data/importedData.json'
 
 test.describe('Verify mock API call', () => {
-    test('Verify mocked post', async ({ page }) => {
-        await page.route('**/users', async (route) => {
-            await route.fulfill({ data });
-        })
-        await page.goto('https://reqres.in/');
-        await expect(page.getByText('morpheus')).toBeVisible();
-    });
-      test('mocks a fruit and does not call api', async ({ page }) => {
-        // Mock the api call before navigating
-        await page.route('*/**/api/v1/fruits', async (route) => {
-          await route.fulfill({ name: 'Strawberry', id: 21 });
-        });
-        // Go to the page
-        await page.goto('https://demo.playwright.dev/api-mocking');
-
-        // Assert that the Strawberry fruit is visible
-        await expect(page.getByText('Strawberry')).toBeVisible();
+    test('mocks a fruit and does not call api', async ({ page }) => {
+      test.setTimeout(120000);
+      // Mock the api call before navigating
+      await page.route('*/**/api/v1/fruits', async (route) => {
+        const json = [{ name: 'Strawberry', id: 21 }];
+        await route.fulfill({ json });
       });
+      // Go to the page
+      await page.goto('https://demo.playwright.dev/api-mocking');
+    
+      // Assert that the Strawberry fruit is visible
+      await expect(page.getByText('Strawberry')).toBeVisible();
+    });
 })
 
---
+++ tests/reqres-mock.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import { data } from '../test-data/importedData.json'
+
+test.describe('Verify mock API call', () => {
+    test('Verify mocked post', async ({ page }) => {
+        await page.route('**/users', async (route) => {
+            await route.fulfill({ data });
+        })
+        await page.goto('https://reqres.in/');
+        await expect(page.getByText('morpheus')).toBeVisible();
+    });
+});
