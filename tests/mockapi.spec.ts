7ab54250f8c7944d89be059c4ffdf9d933cc2435
AJ <andrzej.jurek.qa@gmail.com>
2024-02-06 14:48:54 +0100

    modified:   tests/mockapi.spec.ts

--- tests/mockapi.spec.ts
+++ tests/mockapi.spec.ts
@@ -1,17 +1,18 @@
 import { test, expect } from '@playwright/test';
+import { data } from '../test-data/importedData.json'
 
 test.describe('Verify mock API call', () => {
     test('Verify mocked post', async ({ page }) => {
         await page.route('**/users', async (route) => {
-            await route.fulfill({ "data": {'name': 'morpheus', 'job': 'leader'}});
+            await route.fulfill({ data });
         })
         await page.goto('https://reqres.in/');
         await expect(page.getByText('morpheus')).toBeVisible();
-    })
+    });
       test('mocks a fruit and does not call api', async ({ page }) => {
         // Mock the api call before navigating
         await page.route('*/**/api/v1/fruits', async (route) => {
-          await route.fulfill({ "name": 'Strawberry', id: 21 });
+          await route.fulfill({ name: 'Strawberry', id: 21 });
         });
         // Go to the page
         await page.goto('https://demo.playwright.dev/api-mocking');
