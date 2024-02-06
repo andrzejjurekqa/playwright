315864e919ba0f220edd833812600b028f5c8fa1
AJ <andrzej.jurek.qa@gmail.com>
2024-02-06 14:26:47 +0100

    modified:   .gitignore 	modified:   package.json 	modified:   tests/login.spec.ts 	modified:   tests/mockapi.spec.ts 	modified:   tests/products.spec.ts

--- tests/mockapi.spec.ts
+++ tests/mockapi.spec.ts
@@ -4,7 +4,7 @@ test.describe('Verify mock API call', () => {
     test('Verify mocked post', async ({ page }) => {
         await page.route('**/api/users', async (route) => {
             const json = [{ "data": {'name': 'morpheus', 'job': 'leader'}}];
-            await route.fulfill(json);
+            await route.fulfill({ json });
         })
         await page.goto('https://reqres.in');
         await expect(page.getByText('morpheus')).toBeVisible();
