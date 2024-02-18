7d61924069e2ae813f475fe6247a1167a44689e6
AJ <andrzej.jurek.qa@gmail.com>
2024-02-18 01:04:53 +0100

    modified:   tests/rahul-app/ClientApp.spec.ts

--- tests/rahul-app/ClientApp.spec.ts
+++ tests/rahul-app/ClientApp.spec.ts
@@ -18,8 +18,45 @@ test('Client App Login', async ({page}) => {
         }
     }
     await page.locator("[routerLink*='cart']").click();
-    await page.locator("div li").first().waitFor();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".cartSection").first().waitFor();
     const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible(); //IMPORTANT
-    expect(bool).toBeTruthy();
+    await expect(bool).toBeTruthy();
     await page.locator("text=Checkout").click();
+    await page.locator("[placeholder*='Country']").pressSequentially('Ind');
+    const dropdown = await page.locator('.ta-results');
+    await dropdown.waitFor();
+    for (let i = 0; i < await dropdown.locator('button').count(); ++i) {
+        if (await dropdown.locator('button').nth(i).textContent() === ' India') {
+            await dropdown.locator('button').nth(i).click();
+        };
+    };
+    await page.locator('input[type="text"]').first().clear();
+    await page.locator('input[type="text"]').first().fill('4542 9931 1234 2293');
+    await page.getByRole('combobox').first().selectOption('07');
+    await page.getByRole('combobox').nth(1).selectOption('30');
+    await page.locator('input[type="text"]').nth(1).fill('121');
+    await page.locator('input[type="text"]').nth(2).fill('Andrzej Jurek');
+    await page.locator('input[type="text"]').nth(2).press('Tab');
+    await page.locator('input[name="coupon"]').fill('cheatcheap');
+    //await page.locator('text=Apply Coupon').first().click();
+    await expect(page.locator('.user__name [type="text"]').first()).toHaveText('anshika@gmail.com');
+    await page.locator("text=PLACE ORDER").click();
+    await expect(page.locator('.hero-primary')).toHaveText(' Thankyou for the order. ');
+    const orderNumber = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
+    console.log(orderNumber);
+    await page.locator('.fa-handshake-o').click();
+    await page.locator('tbody').waitFor();
+    const rows = await page.locator('tbody tr');
+
+    for (let i = 0; i < await rows.count(); ++i) {
+        const rowOrderNumber = await page.locator('tr').textContent();
+        if (rowOrderNumber!.includes(orderNumber!)) {
+            //console.log('Found');
+            await rows.nth(i).locator('button').first().click();
+        };
+    };
+
+    await expect(page.locator('.col-text')).toHaveText(orderNumber!);
+    await expect(page.locator('.tagline')).toHaveText('Thank you for Shopping With Us');
 });
