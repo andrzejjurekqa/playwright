f94d5bbc3e175365a95c4c1ce357426ad11f6980
AJ <andrzej.jurek.qa@gmail.com>
2024-02-24 17:01:31 +0100

    modified:   state.json 	new file:   tests/qacart-todo/qacart-e2e.spec.ts 	modified:   tests/qacart-todo/qacart-registration.spec.ts

---
+++ tests/qacart-todo/qacart-e2e.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+import { beforeEach } from "node:test";
+
+test.describe('E2E', () => {
+    test('Valid registration', async ({ page }) => {
+        await page.goto('https://todo.qacart.com/signup')
+        await expect(page.locator('.ktReoZ')).toHaveText('Register to Application');
+        let userName = 'TestName' + Math.floor(Math.random() * 1000);
+        await page.locator('input').nth(0).fill(userName);
+        await page.locator('input').nth(1).fill('Test Surname');
+        await page.locator('input').nth(2).fill(userName + '@gmail.com');
+        await page.locator('input').nth(3).fill('InvalidPassword');
+        await page.locator('input').nth(4).fill('InvalidPassword');
+        await page.getByRole('button').filter({ hasText: "Signup" }).click();
+        await expect(page.locator('.sc-dIouRR')).toHaveText('Good afternoon ' + userName);
+
+        await page.getByTestId('add').click();
+        await expect(page.getByTestId('header')).toHaveText('Create a new Todo');
+        await page.locator('input').fill('To do' + userName);
+        await page.getByTestId('submit-newTask').click();
+        await expect(page.locator('.sc-evZas')).toHaveText('To do' + userName);
+
+        await page.getByRole('checkbox').click();
+        await expect(page.getByRole('checkbox')).toBeChecked();
+        await page.getByTestId('delete').click();
+        await expect(page.getByTestId('todo-item')).toHaveCount(0);
+    });
+});
+
--- tests/qacart-todo/qacart-registration.spec.ts
+++ tests/qacart-todo/qacart-registration.spec.ts
@@ -6,18 +6,27 @@ test.describe('Registration Screen Test', () =>{
         await page.goto('https://todo.qacart.com/signup')
         await expect(page.locator('.ktReoZ')).toHaveText('Register to Application');
     });
-    test('No email', async ({page}) => {
+    test('No email, pass, pass2', async ({page}) => {
         await page.locator('input').nth(0).fill('Test Name');
         await page.locator('input').nth(1).fill('Test Surname');
         await page.getByRole('button').filter({ hasText: "Signup" }).click();
-        await expect(page.locator('.MuiFormHelperText-root').nth(2)).toHaveText('Please Insert a correct Email format');
-    });
-    test('No password', async ({ page }) => {
-        await page.locator('input').nth(0).fill('Test Name');
-        await page.locator('input').nth(1).fill('Test Surname');
+        await expect(page.locator('.MuiFormHelperText-root')).toHaveText('Please Insert a correct Email format');
         await page.locator('input').nth(2).fill('Invalid@gmail.com');
         await page.getByRole('button').filter({ hasText: "Signup" }).click();
         await expect(page.locator('.MuiFormHelperText-root')).toHaveText('Password must be Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character');
+        await page.locator('input').nth(3).fill('InvalidPassword');
+        await page.getByRole('button').filter({ hasText: "Signup" }).click();
+        await expect(page.locator('.MuiFormHelperText-root')).toHaveText('Second password does not match the first Password');
+    });
+    test('Valid registration', async ({ page }) => {
+        let userName = 'TestName' + Math.floor(Math.random() * 1000);
+        await page.locator('input').nth(0).fill(userName);
+        await page.locator('input').nth(1).fill('Test Surname');
+        await page.locator('input').nth(2).fill(userName + '@gmail.com');
+        await page.locator('input').nth(3).fill('InvalidPassword');
+        await page.locator('input').nth(4).fill('InvalidPassword');
+        await page.getByRole('button').filter({ hasText: "Signup" }).click();
+        await expect(page.locator('.sc-dIouRR')).toHaveText('Good afternoon ' + userName);
     });
 });
 
