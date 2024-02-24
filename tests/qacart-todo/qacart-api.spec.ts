c9ebf5ee40a93485e7e88bb163dcabf19fc661fd
AJ <andrzej.jurek.qa@gmail.com>
2024-02-24 15:23:08 +0100

    renamed:    pages/qacart-api.page.ts -> pages/qacartapi.page.ts 	renamed:    pages/rahul-api.page.ts -> pages/rahulapi.page.ts 	renamed:    pages/rahul-cart.page.ts -> pages/rahulcart.page.ts 	modified:   tests/qacart-todo/qacart-api.spec.ts

--- tests/qacart-todo/qacart-api.spec.ts
+++ tests/qacart-todo/qacart-api.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect, request } from "@playwright/test";
-import { APIqaCart } from "../../pages/qacart-api.page";
+import { APIqaCart } from "../../pages/qacartapi.page";
 const regPayload = { email: "newemail3@email.com", password: "Password1234", firstName: "New Name", lastName: "New LastNAme" }
 const logPayload = { email: "newemail3@email.com", password: "Password1234"};
 const taskPayload = { item: "wert", isCompleted: false }; 
